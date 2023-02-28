5261875a00f456ef28a436e76a4ae24bd0032b7c
Kylo Jorgensen <kylo.j.jorgensen@gmail.com>
2023-02-28 05:34:45 +0000

    update favicon title and description

---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+import { api } from "~/utils/api";
+
+const Home: NextPage = () => {
+  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  return (
+    <>
+      <Head>
+        <title>Sweet Apple Acres</title>
+        <meta name="description" content="🍎 Sweet Apple Acres" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 "></div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
