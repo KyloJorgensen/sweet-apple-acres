ac4dc9f77db1c6f740f8c35794f8283e410dcbdd
Kylo Jorgensen <kylo.j.jorgensen@gmail.com>
2023-02-28 06:35:50 +0000

    start

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,7 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           Products
+          <ui></ui>
         </div>
       </main>
     </>
