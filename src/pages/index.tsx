aeb9563b992c301e35740c2a03b598b147eb2aad
Kylo Jorgensen <kylo.j.jorgensen@gmail.com>
2023-02-28 06:33:27 +0000

    cypress working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,9 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 "></div>
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          Products
+        </div>
       </main>
     </>
   );
